3fabc1271c6699b4263c8c9171f6cd2cbd7515dd
Antor Biswas <you@example.combiswasantor20@gmail.com>
2021-02-23 00:09:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let personInfo={
+    name:"Dr. Mahfuz Rahman",
+    job:"Singer",
+    jobLocation:"Dhaka"
+  }
+  let style={
+    color:`goldenrod`,
+    backgroundColor:`blue`,
+    padding:`20px`,
+    borderRadius:`4px`
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3 style={style}>Hey I Am {`${personInfo.name} And I Am A ${personInfo.job}`}</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{color:'yellow'}}>My First React Js</p>
       </header>
     </div>
   );
